2ad4e1bfdf9a0dac900f5ccc4ff9629efbf0bc74
imkumardeepak <deepakraznish@gmail.com>
2025-04-05 12:43:29 +0530

    configchanges

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: [
-      "61f0-106-216-254-170.ngrok-free.app", // Your Ngrok URL
-      ".ngrok-free.app", // Allow all Ngrok subdomains
-      ".trycloudflare.com", // Allow Cloudflare Tunnel (optional)
       "qrlink-demo-app.onrender.com",
+      "atcplagarodemo.vercel.app",
+      "localhost",
+      ".ngrok-free.app",
+      ".trycloudflare.com",
     ],
+    proxy: {
+      "/api": {
+        target: "https://qrlink-demo-app.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
